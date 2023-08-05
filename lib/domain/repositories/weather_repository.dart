ef910eadf506c68857778f82e26072465a62e9c0
Damian Faron <damian.faron@onet.pl>
2023-08-05 12:26:51 +0200

    add from json metod

--- lib/domain/repositories/weather_repository.dart
+++ lib/domain/repositories/weather_repository.dart
@@ -2,22 +2,20 @@ import 'package:simple_weather/data/remote_data_source/weather_remote_data__sour
 import 'package:simple_weather/domain/models/weather_model.dart';
 
 class WeatherRepository {
-  
   WeatherRepository(this._weatherRemoteDataSource);
-  final WeatherRemoteDataSource _weatherRemoteDataSource;
 
+  final WeatherRemoteDataSource _weatherRemoteDataSource;
 
   Future<WeatherModel?> getWeatherModel({
     required String city,
   }) async {
-    final responseData =
+    final json =
         await _weatherRemoteDataSource.getWeatherData(city: city);
-    if (responseData == null) {
+    if (json == null) {
       return null;
     }
-    final name = responseData['location']['name'] as String;
-    final temperature = (responseData['current']['temp_c'] + 0.0) as double;
 
-    return WeatherModel(city: name, temperature: temperature);
+    return WeatherModel.fromJson(json);
+
   }
 }
